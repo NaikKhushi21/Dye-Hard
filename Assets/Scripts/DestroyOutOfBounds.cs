1c8749faa67c3f981254b196af17c038b9ba7b7e
yingtu35 <daniel860305@gmail.com>
2024-09-26 16:18:37 -0700

    objects fall and destroy when out of bounds

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
+    private float topBoundary = 25.0f;
+    private float leftBoundary = -30;
+    private float rightBoundary = 30;
+    private float bottomBoundary = -25.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,25 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameObject.CompareTag("ball") && IsBallOutOfBounds())
+        {
+            Destroy(gameObject);
+        }
+        else if (IsFallingObject() && IsFallingObjectOutOfBounds())
+        {
+            Destroy(gameObject);
+        }
     }
+
+    private bool IsBallOutOfBounds()
+    {
+        return transform.position.z > topBoundary || transform.position.x < leftBoundary || transform.position.y > rightBoundary;
+    }
+
+    private bool IsFallingObject()
+    {
+        return gameObject.CompareTag("Glass") || gameObject.CompareTag("Obstacle");
+    }
+
+    private bool IsFallingObjectOutOfBounds() => transform.position.z < bottomBoundary;
 }
